0a1abcf99ee31fa8f2e6558d13ba3f18d2c3d032
abhijitaj1997 <abhijit.aj.1997@gmail.com>
2026-09-05 19:36:35 +0200

    typed out the new goal

--- CMAbelianVarieties/AbelianVarieties/Homomorphisms.lean
+++ CMAbelianVarieties/AbelianVarieties/Homomorphisms.lean
@@ -2,13 +2,21 @@
 import CMAbelianVarieties.AbstractNonsense.EndomorphismRing
 import Mathlib.AlgebraicGeometry.Group.Abelian
 
-open CategoryTheory AlgebraicGeometry
+open CategoryTheory AlgebraicGeometry Mon
 
 variable {K} [Field K]
 variable (A : Over (Spec (.of K)))
 variable [IsProper A.hom] [GeometricallyIntegral A.hom] [GrpObj A]
 
 instance : IsCommMonObj A := isCommMonObj_of_isProper_of_geometricallyIntegral A
+
+#synth Module ℤ (Hom (mk A) (mk A))
+
+/-
+## Main goal
+The main goal of this section is to show that the homomorphisms between abelian
+varieties give a finite free module.
+-/
 
 
 #min_imports
